d8bb0dd5eae20178685ce6661a82a204fb4ce8c3
farhanrauf27 <farhanrauf27@gmail.com>
2023-09-03 21:01:46 +0500

    3/9/23

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   
   return (
     <>
-    <div className="yoxvv"></div>
+    <div className="yoxvvfr"></div>
     <div className="container-fluid my-2"  >
       <h1 style={{textAlign:'center'}} >Add Task</h1>
     <AddTask editTask={editTask} updateTask={update} addTask={addTask}></AddTask>
